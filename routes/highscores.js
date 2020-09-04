2f00baf4cae84baef1bd9dd201d94a2470c0bced
seunbayo <seunbayo83@gmail.com>
2020-09-04 17:00:09 +0100

    added hiscore fix

--- routes/highscores.js
+++ routes/highscores.js
@@ -6,22 +6,29 @@ var middleware = require("../middleware/index");
 
 
 
-
 //HIGHSCORE ROUTE
-router.get("/end", middleware.isLoggedIn, function (req, res) {
-    let callback = (highscores) => {
-      res.render("end", { highscores });
-    };
-    codeToRetrieveHighScoresFromDb(callback);
-  });
+// router.get("/end", middleware.isLoggedIn, function (req, res) {
+//     let callback = (highscores) => {
+//       res.render("end", { highscores });
+//     };
+//     codeToRetrieveHighScoresFromDb(callback);
+//   });
   
-  router.post("/highscores", function (req, res) {
-    let callback = () => {
-      res.redirect("/highscores");
-    };
-    codeTopostNewHighScore(re.params.username, req.params.score, callback);
-  });
+//   router.post("/highscores", function (req, res) {
+//     let callback = () => {
+//       res.redirect("/highscores");
+//     };
+//     codeTopostNewHighScore(re.params.username, req.params.score, callback);
+//   });
+router.post('/highscores', Middleware(async (req, res, next) => {
+  var { email, score } = req.body;
+  await User.updateOne({ email }, { highScore: score });
 
+}));
+ 
+router.get('/end', Middleware(async (req, res, next) => {
+  var users = await User.find({}, 'name highScore -_id').sort({ highScore: -1}).limit(10);
+}));
 
 
 module.exports = router;
