ab4e9d2063e8cf5638c2ccb51b42308a0f6c681e
seunbayo <seunbayo83@gmail.com>
2020-10-23 11:53:03 +0100

    fixed bugs

--- routes/highscores.js
+++ routes/highscores.js
@@ -3,14 +3,22 @@ var router = express.Router();
 var User = require("../models/user");
 // var middleware = require("../middleware/index");
 
-router.get("/highscores", async (req, res, next) => {
+router.get("/highscores", (req, res, next) => {
   console.log("helo", req.user);
 
   /* if (req.user !== undefined) {
     updateHighscoreForUser(req.user._id, req.query.score);
   } */
 
-  res.render("highscores", { users: topTenUsers() });
+  // res.render("highscores", { users: await topTenUsers() });
+  topTenUsers ()
+    .then((users) => {
+      console.log('promise result');
+      
+      res.render("highscores", { users });
+    })
+    console.log('bye');
+    
 });
 
 async function updateHighscoreForUser(userId, score) {
@@ -23,12 +31,12 @@ async function updateHighscoreForUser(userId, score) {
   }
 }
 
-async function topTenUsers() {
+function topTenUsers() {
   //use the find method on the User model to search for in the database.
-  
-  var result = await User.find().sort({ highScore: -1 }).limit(10);
-    console.log("hi", result.forEach)
-    return result;
+
+  var result = User.find().sort({ highScore: -1 }).limit(10);
+  console.log("hi", result);
+  return result;
 }
 
 module.exports = router;
